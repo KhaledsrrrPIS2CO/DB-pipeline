d549fc8d24e04b5d17fc8a81b76c08074549e72a
Khaled Ameen <khaled.ameen.rrr@gmail.com>
2026-04-23 09:34:17 +0200

    Fix: Glue IAM + Iceberg Catalog - Pipeline Working End-to-End

--- infra/iam.tf
+++ infra/iam.tf
@@ -32,8 +32,10 @@ resource "aws_iam_role_policy" "glue_s3" {
           "s3:ListBucket"
         ]
         Resource = [
-          aws_s3_bucket.bronze.arn,
-          "${aws_s3_bucket.bronze.arn}/*"
+            aws_s3_bucket.bronze.arn,
+            "${aws_s3_bucket.bronze.arn}/*",
+            aws_s3_bucket.silver.arn,
+            "${aws_s3_bucket.silver.arn}/*"
         ]
       }
     ]
@@ -44,4 +46,30 @@ resource "aws_iam_role_policy" "glue_s3" {
 resource "aws_iam_role_policy_attachment" "glue_cloudwatch" {
   role       = aws_iam_role.glue.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+# Allow Glue to use AWS Glue Data Catalog for Iceberg metadata
+resource "aws_iam_role_policy" "glue_catalog" {
+  name = "db-pipeline-glue-catalog"
+  role = aws_iam_role.glue.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "glue:GetDatabase",
+          "glue:CreateDatabase",
+          "glue:GetTable",
+          "glue:CreateTable",
+          "glue:UpdateTable",
+          "glue:DeleteTable",
+          "glue:GetTables",
+          "glue:GetDatabases"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
